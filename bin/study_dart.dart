aa95665155c791acfe81393e3f1a7ddfd32e08cd
nochso <nochso890@gmail.com>
2024-04-09 19:03:26 +0900

    init dart project

---
+++ bin/study_dart.dart
@@ -0,0 +1,8 @@
+import 'package:study_dart/study_dart.dart' as study_dart;
+import 'package:study_dart/VarKeyword.dart' as var_keyword;
+
+void main(List<String> arguments) {
+  print('Hello world: ${study_dart.calculate()}!');
+  print('VarKeyWord_getName : ${var_keyword.getName()}');
+  print('VarKeyWord_getName : ${var_keyword.getDynamicType()}');
+}
---
+++ lib/VarKeyword.dart
@@ -0,0 +1,31 @@
+String getName(){
+    var name = 'Only String'; //String 으로 직접 선어해줘도 된다.
+  // name = 1; 위에 선언타입과 다른 타입을 넣으려 하면 에러발생한다.(int,boolean,...)
+    name = 'TestUser';
+  return name;
+}
+
+getDynamicType(){
+  var name; //변수선언시 초기화하지 않으면 dynamic type 이 된다.
+  name = '';
+  name =12;
+  name = true;
+
+  dynamic something;
+  if(something is String){ //원하는 타입인지 if 문을 이용하여 확인 가능.
+    something.toString(); //해당 타입일 경우 사용 할 수 있는 함수 제공.
+  }
+  something = 12;
+  something = '';
+  something = true;
+  return something;
+}
+
+nullSafety(){
+  String? name = 'user'; // ? 가 없으면 name = null 에 에러 발생.
+  name = null;
+  //name.isNotEmpty; name 이 null 일수도 있기 때문에 에러 발생.
+  if(name != null){
+    name.isNotEmpty;
+  }
+}
