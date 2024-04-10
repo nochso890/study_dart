504accdd95669290acae6d47782581325c2be97d
nochso <nochso890@gmail.com>
2024-04-11 08:40:27 +0900

    variables keyword

--- lib/VarKeyword.dart
+++ lib/VarKeyword.dart
@@ -22,11 +22,31 @@ getDynamicType(){
   return something;
 }
 
-nullSafety(){
+bool nullSafety(){
   String? name = 'user'; // ? 가 없으면 name = null 에 에러 발생.
   name = null;
   //name.isNotEmpty; name 이 null 일수도 있기 때문에 에러 발생.
   if(name != null){
     name.isNotEmpty;
+    return true;
   }
+  return false;
+}
+
+finalVariables(){
+  final String name = 'user'; //final 을 선언이 후 에는 변수를 수정 할 수 없다.
+  final age = 12; //final 뒤에 타입은 Dart 에서 변수값에 맞게 타입이 지정되기 때문에 생략 할 수 있다.
+  // name = 'change';
+}
+
+lateVariables(){
+  late final String name; //초기 데이터 없이 선언 할 수 있게 해준다. (데이터를 API 요청 후 나중에 넣어야 할 경우 사용)
+  // print(name); 데이터 초기값 입력전에 출력 요청을 작성하면 에러 발생.
+  //do something, go to api
+  name = 'user'; //1회 한해서 초기 데이터를 입력 할 수 있다.
+}
+
+constVariables(){
+  const name = 'user'; // 컴파일시에 하드코드 되어 값이 변하지 않는다.
+  //name = '12'; final 과 같이 선언 이후에는 수정되지 않는다.
 }
